32c46171984b17f787c94881c401f4aa0c1cdcf8
Patrick O'Connor <pattyo20@optonline.net>
2023-12-21 16:14:57 -0500

    cleaned up the forms, working on keyboard functionality

--- src/components/Landingpage/LandingPage.tsx
+++ src/components/Landingpage/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import {
   View,
   Text,
@@ -7,7 +7,7 @@ import {
   StyleSheet,
   KeyboardAvoidingView,
   Platform,
-  Keyboard,
+  TouchableWithoutFeedback,
 } from 'react-native';
 
 // Styles
@@ -66,6 +66,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 10,
     padding: 10,
+    backgroundColor: '#fff',
   },
 });
 
@@ -83,6 +84,13 @@ function LandingPage() {
     password: '',
   });
 
+  const loginInputRef = useRef<TextInput>(null);
+
+  const registerNameInputRef = useRef<TextInput>(null);
+  const registerEmailInputRef = useRef<TextInput>(null);
+  const registerUsernameInputRef = useRef<TextInput>(null);
+  const registerPasswordInputRef = useRef<TextInput>(null);
+
   const handleRegistration = async () => {
     // Implement registration logic for React Native
   };
@@ -95,106 +103,123 @@ function LandingPage() {
   const dismissForms = () => {
     setShowLoginForm(false);
     setShowRegisterForm(false);
-    Keyboard.dismiss();
   };
 
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={styles.container}>
-      <Text style={styles.title}>Welcome to Old Man Hockey!</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={styles.loginButton}
-          onPress={() => {
-            setShowLoginForm(true);
-            setShowRegisterForm(false);
-            Keyboard.dismiss();
-          }}>
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.registerButton}
-          onPress={() => {
-            setShowLoginForm(false);
-            setShowRegisterForm(true);
-            Keyboard.dismiss();
-          }}>
-          <Text style={styles.buttonText}>Register</Text>
-        </TouchableOpacity>
-      </View>
+      <TouchableWithoutFeedback onPress={dismissForms}>
+        <View style={styles.container}>
+          <Text style={styles.title}>Welcome to Old Man Hockey!</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.loginButton}
+              onPress={() => {
+                setShowLoginForm(true);
+                setShowRegisterForm(false);
+                if (loginInputRef.current) {
+                  loginInputRef.current.focus();
+                }
+              }}>
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.registerButton}
+              onPress={() => {
+                setShowLoginForm(false);
+                setShowRegisterForm(true);
+                if (registerNameInputRef.current) {
+                  registerNameInputRef.current.focus();
+                }
+              }}>
+              <Text style={styles.buttonText}>Register</Text>
+            </TouchableOpacity>
+          </View>
 
-      {showLoginForm && (
-        <View style={styles.formContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Username"
-            value={loginData.username}
-            onChangeText={text => setLoginData({...loginData, username: text})}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Password"
-            secureTextEntry
-            value={loginData.password}
-            onChangeText={text => setLoginData({...loginData, password: text})}
-          />
-          <TouchableOpacity
-            style={styles.loginButton}
-            onPress={() => {
-              handleLogin();
-              dismissForms();
-            }}>
-            <Text style={styles.buttonText}>Login</Text>
-          </TouchableOpacity>
-        </View>
-      )}
+          {showLoginForm && (
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder="Username"
+                value={loginData.username}
+                onChangeText={text =>
+                  setLoginData({...loginData, username: text})
+                }
+                ref={loginInputRef}
+                autoFocus={true}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry
+                value={loginData.password}
+                onChangeText={text =>
+                  setLoginData({...loginData, password: text})
+                }
+                ref={loginInputRef}
+              />
+              <TouchableOpacity
+                style={styles.loginButton}
+                onPress={() => {
+                  handleLogin();
+                }}>
+                <Text style={styles.buttonText}>Login</Text>
+              </TouchableOpacity>
+            </View>
+          )}
 
-      {showRegisterForm && (
-        <View style={styles.formContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Name"
-            value={registrationData.name}
-            onChangeText={text =>
-              setRegistrationData({...registrationData, name: text})
-            }
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Email"
-            value={registrationData.email}
-            onChangeText={text =>
-              setRegistrationData({...registrationData, email: text})
-            }
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Username"
-            value={registrationData.username}
-            onChangeText={text =>
-              setRegistrationData({...registrationData, username: text})
-            }
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Password"
-            secureTextEntry
-            value={registrationData.password}
-            onChangeText={text =>
-              setRegistrationData({...registrationData, password: text})
-            }
-          />
-          <TouchableOpacity
-            style={styles.registerButton}
-            onPress={() => {
-              handleRegistration();
-              dismissForms();
-            }}>
-            <Text style={styles.buttonText}>Register</Text>
-          </TouchableOpacity>
+          {showRegisterForm && (
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder="Name"
+                value={registrationData.name}
+                onChangeText={text =>
+                  setRegistrationData({...registrationData, name: text})
+                }
+                ref={registerNameInputRef}
+                autoFocus={true}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Email"
+                value={registrationData.email}
+                onChangeText={text =>
+                  setRegistrationData({...registrationData, email: text})
+                }
+                ref={registerEmailInputRef}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Username"
+                value={registrationData.username}
+                onChangeText={text =>
+                  setRegistrationData({...registrationData, username: text})
+                }
+                ref={registerUsernameInputRef}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry
+                value={registrationData.password}
+                onChangeText={text =>
+                  setRegistrationData({...registrationData, password: text})
+                }
+                ref={registerPasswordInputRef}
+              />
+              <TouchableOpacity
+                style={styles.registerButton}
+                onPress={() => {
+                  handleRegistration();
+                }}>
+                <Text style={styles.buttonText}>Register</Text>
+              </TouchableOpacity>
+            </View>
+          )}
         </View>
-      )}
+      </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
 }
