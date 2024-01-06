9e0783ecabc55b78c6aa23accc33a27a93527789
Patrick O'Connor <pattyo20@optonline.net>
2024-01-06 15:02:11 -0500

    changed the name on everything to BetterPlay. Database URL had to stay as OMHL though

--- android/app/src/main/java/com/omhl/MainApplication.kt
+++ android/app/src/main/java/com/omhl/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.omhl
+package com.BetterPlay
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/BetterPlayTests/OMHLTests.m
+++ ios/BetterPlayTests/OMHLTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface OMHLTests : XCTestCase
+@interface BetterPlayTests : XCTestCase
 
 @end
 
-@implementation OMHLTests
+@implementation BetterPlayTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
