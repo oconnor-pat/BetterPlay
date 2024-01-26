5b6733955c9ee6b041d591daad8b7793d2d53225
Patrick O'Connor <pattyo20@optonline.net>
2024-01-26 16:51:33 -0500

    fixed the icons and some other stuff

--- src/components/BottomNavigator/BottomNavBar.tsx
+++ src/components/BottomNavigator/BottomNavBar.tsx
@@ -3,54 +3,60 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Roster from '../Roster/Roster';
 import Profile from '../Profile/Profile';
 import CommunityNotes from '../Communitynotes/CommunityNotes';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {
+  faUsers,
+  faUser,
+  faStickyNote,
+  faQuestion,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Tab = createBottomTabNavigator();
 
-const iconMap: Record<string, string> = {
-  Roster: 'team',
-  Profile: 'user',
-  CommunityNotes: 'sticky-note',
+const iconMap: Record<string, any> = {
+  Roster: faUsers,
+  Profile: faUser,
+  CommunityNotes: faStickyNote,
 };
 
-const TabBarIcon = (props: {name: string; color: string; size: number}) => {
-  const {name, color, size} = props;
-  const iconName = iconMap[name] || 'user';
-  return <Icon name={iconName} size={size} color={color} />;
+const TabBarIcon = (props: {icon: any; color: string; size: number}) => {
+  const {icon, color, size} = props;
+  return <FontAwesomeIcon icon={icon} size={size} color={color} />;
 };
 
-const TabBarIconWrapper = ({
+const TabBarIconComponent = ({
   route,
-  ...props
+  color,
+  size,
 }: {
   route: any;
   color: string;
   size: number;
 }) => {
-  return <TabBarIcon name={route.name} {...props} />;
+  const icon = iconMap[route.name] || faQuestion;
+  return <TabBarIcon icon={icon} color={color} size={size} />;
 };
 
-const screenOptions = (route: any) => ({
+const screenOptions = ({route}: {route: any}) => ({
   headerShown: false,
+  tabBarLabel: () => null,
   tabBarStyle: {
     backgroundColor: '#02131D',
     borderTopColor: '#fff',
     paddingBottom: 10,
     paddingTop: 10,
   },
-  tabBarIcon: (props: any) => <TabBarIconWrapper route={route} {...props} />,
+  tabBarIcon: (props: {color: string; size: number}) => (
+    <TabBarIconComponent route={route} {...props} />
+  ),
 });
 
 const BottomNavBar: React.FC = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Roster" component={Roster} options={screenOptions} />
-      <Tab.Screen name="Profile" component={Profile} options={screenOptions} />
-      <Tab.Screen
-        name="CommunityNotes"
-        component={CommunityNotes}
-        options={screenOptions}
-      />
+    <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen name="Roster" component={Roster} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="CommunityNotes" component={CommunityNotes} />
     </Tab.Navigator>
   );
 };
